2e2dc6440e4ea32e8e2aad2a2ffafb09c3b8a2ad
MarcoGranata9 <granatam9900@gmail.com>
2024-05-28 14:58:55 +0200

    add: calculator tester

--- src/main/java/org/learning/calculator/Calculator.java
+++ src/main/java/org/learning/calculator/Calculator.java
@@ -1,2 +1,23 @@
-package org.learning.calculator;public class Calculator {
+package org.learning.calculator;
+
+public class Calculator {
+    // Metodi
+    public float add(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public float divide(float num1, float num2) throws IllegalArgumentException{
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Cannot divide by 0");
+        }
+       return num1 / num2;
+    }
+
+    public float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
 }
--- src/test/java/org/learning/calculator/CalculatorTest.java
+++ src/test/java/org/learning/calculator/CalculatorTest.java
@@ -1,4 +1,34 @@
+package org.learning.calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+
+    @Test
+    void add() {
+        Calculator calc = new Calculator();
+        assertEquals((float) 4.9, calc.add((float) 1.5 ,(float) 3.4));
+    }
+
+    @Test
+    void subtract() {
+        Calculator calc = new Calculator();
+        assertEquals( 0.9f, calc.subtract( 2.5f , 1.6f));
+    }
+
+    @Test
+    void divide() {
+        Calculator calc = new Calculator();
+        assertEquals((float) 2.5, calc.divide(5, 2));
+        assertThrows(IllegalArgumentException.class, ()-> calc.divide(4, 0));
+    }
+
+    @Test
+    void multiply() {
+        Calculator calc = new Calculator();
+        assertEquals(10.6f, calc.multiply(5.3f, 2));
+    }
 }
